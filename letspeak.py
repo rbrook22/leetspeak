7d656ea9dace70675c663d4e99843f5f43748958
Richard Brooks <richardbrooks@Richards-MacBook-Pro.local>
2018-04-30 10:07:19 -0400

    updated for statement

--- letspeak.py
+++ letspeak.py
@@ -1,1 +1,12 @@
-print ('hello world')
+# configuration
+user_input = input('Type a word: ')
+user_input = user_input.upper()
+alphabet = ['A', 'E', 'G', 'I', 'O', 'S', 'T']
+num = [4, 3, 6, 1, 0, 5, 7]
+
+# process
+for i in user_input:
+  if i in alphabet:
+      print (num[alphabet.index(i)])
+  else:
+        print(i)
